8447375f511a9088e256342f98acd12039925344
halko <adrian.halko@ringier.sk>
2024-06-27 15:03:03 +0200

    update card form design

--- payu_add_card/lib/src/widget/add_card_widget.dart
+++ payu_add_card/lib/src/widget/add_card_widget.dart
@@ -38,74 +38,85 @@ class AddCardWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: PayuPadding.padding8),
-                child: Row(
-                  children: [
-                    Text(
-                      'new_card'.translated(),
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: PayuPadding.padding16),
-                        child: Container(
-                          height: 1,
-                          color: PayuColors.secondaryGray3,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: PayuPadding.padding8,
-                ),
-                child: Wrap(
-                  spacing: PayuPadding.padding16,
-                  children: PaymentCardProvider.all()
-                      .map(
-                        (e) => Opacity(
-                          opacity: e == controller.cardProvider ? 1 : 0.25,
-                          child: PayuFadeInImage(uri: e.brandImageProvider.light, size: 48),
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(vertical: PayuPadding.padding8),
+              //   child: Row(
+              //     children: [
+              //       Text(
+              //         'new_card'.translated(),
+              //         style: Theme.of(context).textTheme.titleMedium,
+              //       ),
+              //       Expanded(
+              //         child: Padding(
+              //           padding: const EdgeInsets.only(left: PayuPadding.padding16),
+              //           child: Container(
+              //             height: 1,
+              //             color: PayuColors.secondaryGray3,
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(
+              //     vertical: PayuPadding.padding8,
+              //   ),
+              //   child: Wrap(
+              //     spacing: PayuPadding.padding16,
+              //     children: PaymentCardProvider.all()
+              //         .map(
+              //           (e) => Opacity(
+              //             opacity: e == controller.cardProvider ? 1 : 0.25,
+              //             child: PayuFadeInImage(uri: e.brandImageProvider.light, size: 48),
+              //           ),
+              //         )
+              //         .toList(),
+              //   ),
+              // ),
+
               AddCardTextField(
                 controller: controller.numberController,
                 decoration: configuration.numberDecoration,
               ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: AddCardTextField(
-                      controller: controller.dateController,
-                      decoration: configuration.dateDecoration,
-                    ),
-                  ),
-                  const SizedBox(width: PayuPadding.padding16),
-                  Expanded(
-                    child: AddCardTextField(
-                      controller: controller.cvvController,
-                      decoration: configuration.cvvDecoration,
-                    ),
-                  ),
-                ],
+              AddCardTextField(
+                controller: controller.dateController,
+                decoration: configuration.dateDecoration,
               ),
-              Visibility(
-                visible: configuration.isFooterVisible,
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: PayuPadding.padding16),
-                  child: SizedBox(
-                    width: double.maxFinite,
-                    child: TermsAndConditionsWidget(),
-                  ),
-                ),
-              ),
+              AddCardTextField(
+                controller: controller.cvvController,
+                decoration: configuration.cvvDecoration,
+              )
+
+
+              // Row(
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+              //   children: [
+              //     Expanded(
+              //       child: AddCardTextField(
+              //         controller: controller.dateController,
+              //         decoration: configuration.dateDecoration,
+              //       ),
+              //     ),
+              //     const SizedBox(width: PayuPadding.padding16),
+              //     Expanded(
+              //       child: AddCardTextField(
+              //         controller: controller.cvvController,
+              //         decoration: configuration.cvvDecoration,
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              // Visibility(
+              //   visible: configuration.isFooterVisible,
+              //   child: const Padding(
+              //     padding: EdgeInsets.symmetric(vertical: PayuPadding.padding16),
+              //     child: SizedBox(
+              //       width: double.maxFinite,
+              //       child: TermsAndConditionsWidget(),
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
